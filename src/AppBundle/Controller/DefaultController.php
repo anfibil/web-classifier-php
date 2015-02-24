d71a2f6d55311c88adbcdbb2df8c423d102b1b8f
Everaldo Aguiar <anfibil@gmail.com>
2015-02-24 00:41:49 -0500

    Testing phpstorm github integration

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends Controller
 
     public function helloAction($name)
     {
+        // Added comment
         return $this->render('default/hello.html.twig',array('name' => $name));
     }
 }
