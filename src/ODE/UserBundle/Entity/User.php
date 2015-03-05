c55067966e7418dba53c0554b39fee20989db600
Everaldo Aguiar <anfibil@gmail.com>
2015-03-05 02:12:08 -0500

    Failed to implement profile picture uploader

--- src/ODE/UserBundle/Entity/User.php
+++ src/ODE/UserBundle/Entity/User.php
@@ -5,11 +5,14 @@ namespace ODE\UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Util\SecureRandom;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="ode_user")
+ * @ORM\HasLifecycleCallbacks
  */
 class User extends BaseUser
 {
@@ -62,27 +65,43 @@ class User extends BaseUser
      */
     protected $affiliation;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $lastEdited;
+
+    /**
+     * @Assert\File(maxSize="2048k")
+     * @Assert\Image(mimeTypesMessage="Please upload a valid image.")
+     */
+    protected $profilePictureFile;
+
+    // for temporary storage
+    private $tempProfilePicturePath;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $profilePicturePath;
+
+    // --------------------//
+    // Getters and Setters //
+    // --------------------//
 
     public function getFirstname()
     {
         return $this->firstname;
     }
 
-    public function getLastname()
-    {
-        return $this->lastname;
-    }
-
-    public function getAffiliation()
-    {
-        return $this->affiliation;
-    }
-
-
     public function  setFirstname($firstname)
     {
         $this->firstname = $firstname;
         return $this;
+    }
+
+        public function getLastname()
+    {
+        return $this->lastname;
     }
 
     public function setLastname($lastname)
@@ -91,14 +110,228 @@ class User extends BaseUser
         return $this;
     }
 
+    public function getAffiliation()
+    {
+        return $this->affiliation;
+    }
+
     public function setAffiliation($affiliation)
     {
         $this->affiliation = $affiliation;
         return $this;
     }
 
+    /**
+     * Asks whether the user is granted a particular role
+     *
+     * @return boolean
+     */
+    public function isGranted($role)
+    {
+        return in_array($role, $this->getRoles());
+    }
+
+    /**
+     * Sets the file used for profile picture uploads
+     *
+     * @param UploadedFile $file
+     * @return object
+     */
+    public function setProfilePictureFile(UploadedFile $file = null) {
+        // set the value of the holder
+        $this->profilePictureFile       =   $file;
+        // check if we have an old image path
+        if (isset($this->profilePicturePath)) {
+            // store the old name to delete after the update
+            $this->tempProfilePicturePath = $this->profilePicturePath;
+            $this->profilePicturePath = null;
+        } else {
+            $this->profilePicturePath = 'initial';
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the file used for profile picture uploads
+     *
+     * @return UploadedFile
+     */
+    public function getProfilePictureFile() {
+
+        return $this->profilePictureFile;
+    }
+
+    /**
+     * Set profilePicturePath
+     *
+     * @param string $profilePicturePath
+     * @return User
+     */
+    public function setProfilePicturePath($profilePicturePath)
+    {
+        $this->profilePicturePath = $profilePicturePath;
+
+        return $this;
+    }
+
+    /**
+     * Get profilePicturePath
+     *
+     * @return string
+     */
+    public function getProfilePicturePath()
+    {
+        return $this->profilePicturePath;
+    }
+
+    /**
+     * Get the absolute path of the profilePicturePath
+     */
+    public function getProfilePictureAbsolutePath() {
+        return null === $this->profilePicturePath
+            ? null
+            : $this->getUploadRootDir().'/'.$this->profilePicturePath;
+    }
+
+    /**
+     * Get root directory for file uploads
+     *
+     * @return string
+     */
+    protected function getUploadRootDir($type='profilePicture') {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir($type);
+    }
+
+    /**
+     * Specifies where in the /web directory profile pic uploads are stored
+     *
+     * @return string
+     */
+    protected function getUploadDir($type='profilePicture') {
+        // the type param is to change these methods at a later date for more file uploads
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'asset/img';
+    }
+
+    /**
+     * Get the web path for the user
+     *
+     * @return string
+     */
+    public function getWebProfilePicturePath() {
+
+        return '/'.$this->getUploadDir().'/'.$this->getProfilePicturePath();
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUploadProfilePicture() {
+        if (null !== $this->getProfilePictureFile()) {
+            // a file was uploaded
+            // generate a unique filename
+            $filename = $this->generateRandomProfilePictureFilename();
+            $this->setProfilePicturePath($filename.'.'.$this->getProfilePictureFile()->guessExtension());
+        }
+    }
+
+    /**
+     * Generates a 32 char long random filename
+     *
+     * @return string
+     */
+    public function generateRandomProfilePictureFilename() {
+        $count                  =   0;
+        do {
+            $generator = new SecureRandom();
+            $random = $generator->nextBytes(16);
+            $randomString = bin2hex($random);
+            $count++;
+        }
+        while(file_exists($this->getUploadRootDir().'/'.$randomString.'.'.$this->getProfilePictureFile()->guessExtension()) && $count < 50);
+
+        return $randomString;
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     *
+     * Upload the profile picture
+     *
+     * @return mixed
+     */
+    public function uploadProfilePicture() {
+        // check there is a profile pic to upload
+        if ($this->getProfilePictureFile() === null) {
+            return;
+        }
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getProfilePictureFile()->move($this->getUploadRootDir(), $this->getProfilePicturePath());
+
+        // check if we have an old image
+        if (isset($this->tempProfilePicturePath) && file_exists($this->getUploadRootDir().'/'.$this->tempProfilePicturePath)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->tempProfilePicturePath);
+            // clear the temp image path
+            $this->tempProfilePicturePath = null;
+        }
+        $this->profilePictureFile = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeProfilePictureFile()
+    {
+        if ($file = $this->getProfilePictureAbsolutePath() && file_exists($this->getProfilePictureAbsolutePath())) {
+            unlink($file);
+        }
+    }
+
+
+
+    /**
+     * Set lastEdited
+     *
+     * @param \DateTime $lastEdited
+     * @return User
+     */
+    public function setLastEdited($lastEdited)
+    {
+        $this->lastEdited = $lastEdited;
+
+        return $this;
+    }
+
+    /**
+     * Get lastEdited
+     *
+     * @return \DateTime
+     */
+    public function getLastEdited()
+    {
+        return $this->lastEdited;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function setLastEditedValueAsNow() {
+        $this->setLastEdited(new \DateTime());
+    }
+
     public function __construct()
     {
         parent::__construct();
+        $this->path = "/assets/img/anonymous.jpg";
     }
 }
--- web/upload.php
+++ web/upload.php
@@ -1,31 +1,9 @@
 <?php
 
-//if they DID upload a file...
-if($_FILES['photo']['name'])
-{
-//if no errors...
-if(!$_FILES['photo']['error'])
-{
-//now is the time to modify the future file name and validate the file
-$new_file_name = strtolower($_FILES['photo']['tmp_name']); //rename file
-if($_FILES['photo']['size'] > (1024000)) //can't be larger than 1 MB
-{
-$valid_file = false;
-$message = 'Oops!  Your file\'s size is to large.';
+if (isset($_POST['uploadButton'])){
+    $file = $_FILES['avatarImage']['tmp_name'];
+    $fileName = $_FILES['avatarImage']['name'];
+    move_uploaded_file($file,"/assets/img/$fileName");
 }
 
-//if the file has passed the test
-if($valid_file)
-{
-//move it to where we want it to be
-move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/'.$new_file_name);
-$message = 'Congratulations!  Your file was accepted.';
-}
-}
-//if there is an error...
-else
-{
-//set that to be the returned message
-$message = 'Ooops!  Your upload triggered the following error:  '.$_FILES['photo']['error'];
-}
-}
+?>
