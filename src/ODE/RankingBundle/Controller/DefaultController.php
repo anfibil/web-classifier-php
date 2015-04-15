4b55121a035ad8f2f301412a3a09eb06f048acbb
Everaldo Aguiar <anfibil@gmail.com>
2015-04-15 02:55:05 -0400

    Minor edit

--- src/ODE/RankingBundle/Controller/DefaultController.php
+++ src/ODE/RankingBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
         $datasets = $em->getRepository('ODEDatasetBundle:Dataset')->findAll();
