c1c6a790a7d7ca25ad7e5c8f2432305e6916fa43
Everaldo Aguiar <anfibil@gmail.com>
2015-04-14 23:16:00 -0400

    Trying to fix ajax problem on ranking page.

--- src/ODE/RankingBundle/Controller/DefaultController.php
+++ src/ODE/RankingBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace ODE\RankingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ODERankingBundle:Default:index.html.twig', array('name' => 'name'));
+        $em = $this->getDoctrine()->getManager();
+        $datasets = $em->getRepository('ODEDatasetBundle:Dataset')->findAll();
+        return $this->render('ODERankingBundle:Default:index.html.twig', array(
+            'datasets' => $datasets
+        ));
+    }
+
+    public function getdataAction($dataset_id)
+    {
+        return "<tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>";
     }
 }
