24a5164d18eedb571bb57f2d33206d09a0c873a3
Everaldo Aguiar <anfibil@gmail.com>
2015-04-15 02:52:10 -0400

    First draft of ranking functionality.

--- src/ODE/RankingBundle/Controller/DefaultController.php
+++ src/ODE/RankingBundle/Controller/DefaultController.php
@@ -3,12 +3,12 @@
 namespace ODE\RankingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $datasets = $em->getRepository('ODEDatasetBundle:Dataset')->findAll();
@@ -17,9 +17,25 @@ class DefaultController extends Controller
         ));
     }
 
-    public function getTableData($dataset_id)
+    public function getTableDataAction(Request $request)
     {
-        $return=json_encode("testt");//jscon encode the array
-        return new Response($return,200,array('Content-Type'=>'application/json'));//make sure it has the correct content type
+        $dataset_id = $request->query->get('dataset_id');
+        $fields = array('r.username', 'r.model', 'r.accuracy', 'r.auroc', 'r.aupr', 'r.runtime');
+
+        $repository = $this->getDoctrine()->getRepository('ODEAnalysisBundle:Result');
+        $query = $repository->createQueryBuilder('r')
+            ->select($fields)
+            ->where('r.dataset = :dataset')
+            ->setParameter('dataset', $dataset_id)
+            ->getQuery();
+
+        //$query->setMaxResults(10);
+        $results = $query->getResult();
+
+        $data = array();
+        foreach ($results as $result){
+            array_push($data,array_values($result));
+        }
+        return new Response(json_encode(array('data'=>$data)));
     }
 }
