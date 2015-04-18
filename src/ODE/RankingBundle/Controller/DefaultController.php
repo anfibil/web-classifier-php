5bbd5e6d58a173f745b5004f3a52cbc71a98c0bc
Renan Gomes <email@renangomes.com>
2015-04-18 01:52:44 -0300

    Rankings on click events and mouse effects

--- src/ODE/RankingBundle/Controller/DefaultController.php
+++ src/ODE/RankingBundle/Controller/DefaultController.php
@@ -44,8 +44,9 @@ class DefaultController extends Controller
             $aupr = $result->getAupr();
             $runtime = $result->getRuntime();
             $date = $result->getDate();
+            $id = $result->getId();
 
-            $result = array($user,$model,$accuracy,$auroc,$aupr,$runtime,$date);
+            $result = array($user,$model,$accuracy,$auroc,$aupr,$runtime,$date,$id);
 
             // Remove keys from array to comply with what datatables expects
             $result = array_values($result);
@@ -72,19 +73,19 @@ class DefaultController extends Controller
         $entityManager = $this->getDoctrine()->getManager();
 
         $accuracy = $entityManager->createQuery(
-            'SELECT U.username as username,R.accuracy as top FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.accuracy DESC, R.id ASC'
+            'SELECT U.username as username,R.accuracy as top, R.id as result_id FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.accuracy DESC, R.id ASC'
         )->setParameter('dataset', $dataset_id)->setMaxResults(1)->getOneOrNullResult();
 
         $auroc = $entityManager->createQuery(
-            'SELECT U.username as username,R.auroc as top FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.auroc DESC, R.id ASC'
+            'SELECT U.username as username,R.auroc as top, R.id as result_id FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.auroc DESC, R.id ASC'
         )->setParameter('dataset', $dataset_id)->setMaxResults(1)->getOneOrNullResult();
 
         $aupr = $entityManager->createQuery(
-            'SELECT U.username as username,R.aupr as top FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.aupr DESC, R.id ASC'
+            'SELECT U.username as username,R.aupr as top, R.id as result_id FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.aupr DESC, R.id ASC'
         )->setParameter('dataset', $dataset_id)->setMaxResults(1)->getOneOrNullResult();
 
         $runtime = $entityManager->createQuery(
-            'SELECT U.username as username,R.runtime as top FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.runtime ASC, R.id ASC'
+            'SELECT U.username as username,R.runtime as top, R.id as result_id FROM ODEAnalysisBundle:Result R JOIN R.user U WHERE R.dataset = :dataset ORDER BY R.runtime ASC, R.id ASC'
         )->setParameter('dataset', $dataset_id)->setMaxResults(1)->getOneOrNullResult();
 
         $response = json_encode(array('accuracy' => $accuracy, 'auroc' => $auroc, 'aupr' =>$aupr, 'runtime' =>$runtime));
