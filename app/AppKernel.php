d2a13f502c40df949b271e570ef71f6cff2e790d
Reid Johnson <rjohns15@nd.edu>
2015-07-10 03:45:25 -0400

    Updated dataset uploading.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new ODE\DatasetBundle\ODEDatasetBundle(),
             new ODE\AnalysisBundle\ODEAnalysisBundle(),
             new ODE\RankingBundle\ODERankingBundle(),
+            //new Vich\UploaderBundle\VichUploaderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
